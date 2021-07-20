3023c51832b45c26f1bf6d7f7d39d274ef9642bd
Long Duong <longduong@kms-technology.com>
2021-07-20 23:45:34 +0700

    Setup hot reloading for SCSS and ES6

---
+++ VMS/Resources/src/common/lodash.js
@@ -0,0 +1,10 @@
+﻿import {
+    cloneDeep,
+    flatten
+} from 'lodash';
+
+const CloneDeep = (object) => cloneDeep(object);
+
+const Flatten = (array) => flatten(array);
+
+export default { CloneDeep, Flatten };
---
+++ VMS/Resources/src/index.js
@@ -0,0 +1,5 @@
+﻿import lodash from './common/lodash';
+
+export const CloneDeep = (object) => lodash.CloneDeep(object);
+
+export const Flatten = (array) => lodash.Flatten(array);

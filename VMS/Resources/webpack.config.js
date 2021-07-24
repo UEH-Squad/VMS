974af22048485fe24633dcc6344e4d3e6eadb56f
longduongkms <85151497+longduongkms@users.noreply.github.com>
2021-07-24 22:36:52 +0700

    Setup hot reloading for SCSS and ES6 (#3)
    * Setup hot reloading for SCSS and ES6
    
    * master | Setup VS Code tasks and launch settings
    
    * Refactoring layout for consistency
    
    * Add local sample logo
    
    * Setup font Montserrat and Material icons
    
    * Fix bugs SCSS
    
    * Add text color var
    
    * Add image materials
    
    Co-authored-by: Long Duong <huulongduong@gmail.com>

---
+++ VMS/Resources/webpack.config.js
@@ -0,0 +1,25 @@
+﻿const path = require("path");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, '../wwwroot/js'),
+        filename: "vms.js",
+        library: "vms"
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 500,
+        ignored: /node_modules/,
+    },
+};

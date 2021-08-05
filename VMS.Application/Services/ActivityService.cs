288b72890b08394d7d18c7897f0b0fc4cd91b56b
PhamVuHoang <hoangpham.31191026947@st.ueh.edu.vn>
2021-08-05 13:32:56 +0700

    save 1

---
+++ VMS.Application/Services/ActivityService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.Application.Interfaces;
+
+namespace VMS.Application.Services
+{
+    class ActivityService : IActivityService
+    {
+
+    }
+}
---
+++ VMS.Application/ViewModels/UserLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMS.Application.ViewModels
+{
+    class UserLocation
+    {
+        public float LatUser { get; set; }
+        public float LonUser { get; set; }
+
+    }
+}

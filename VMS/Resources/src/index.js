c4da8fa89b3e57380229597c75015554ad1290f9
lananhkn <87310006+lananhkn@users.noreply.github.com>
2021-08-08 15:32:20 +0700

    Add Related Activities Section (#45)
    * Add related activities section
    
    * add get user location function
    
    * delete property
    
    * adjusted as Mr.Long's requirements
    
    * fix responsive
    
    * fix the position of class act-more-2
    
    * fix responsive
    
    * add overflow-hidden
    
    * add width
    
    * fix responsive
    
    * add width 100%
    
    * change the position of 'nhieu hon'
    
    * just save it the last time, nothing was changed
    
    * save code the last time
    
    * move related activities under filter

--- VMS/Resources/src/index.js
+++ VMS/Resources/src/index.js
@@ -5,3 +5,4 @@ export const CloneDeep = (object) => lodash.CloneDeep(object);
 export const PlayVideo = (src) => homepage.playVideo(src);
 export const FilterCarousel = () => homepage.filterCarousel();
 export const LogoBanerCarousel = () => homepage.logoBanerCarousel();
+export const GetUserLocation = () => homepage.getUserLocation();

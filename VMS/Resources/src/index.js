1755abdca92442fc665defa4abae5650c8153b3c
Bằng Thái Ngọc Hữu <thaingochuubang10@gmail.com>
2021-08-20 06:12:48 +0700

    fix name component, modal

--- VMS/Resources/src/index.js
+++ VMS/Resources/src/index.js
@@ -1,6 +1,5 @@
 ﻿import lodash from './common/lodash';
 import homepage from './homepage';
-import act from './act';
 
 export const CloneDeep = (object) => lodash.CloneDeep(object);
 export const PlayVideo = (src) => homepage.playVideo(src);
@@ -8,5 +7,4 @@ export const FilterCarousel = () => homepage.filterCarousel();
 export const LogoBanerCarousel = () => homepage.logoBanerCarousel();
 export const GetUserLocation = () => homepage.getUserLocation();
 export const ShowResult = () => homepage.showResult();
-export const ShowColor = () => act.showColor();
 

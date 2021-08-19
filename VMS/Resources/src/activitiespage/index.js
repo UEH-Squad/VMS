9c0a5a40107d14a884684fa07752a986c7dac02a
Truckhang181001 <Truckhang181001@gmail.com>
2021-08-19 22:45:46 +0700

    Finish frontend of other-activities. I cant handle favorite icon click issue although i tried

---
+++ VMS/Resources/src/activitiespage/index.js
@@ -0,0 +1,19 @@
+﻿const otherAct = () => {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<span class="material-icons">navigate_before</span>',
+            '<span class="material-icons">navigate_next</span>'
+        ],
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+        }
+    })
+}
+
+export default { otherAct };
--- VMS/Resources/src/index.js
+++ VMS/Resources/src/index.js
@@ -1,5 +1,6 @@
 ﻿import lodash from './common/lodash';
 import homepage from './homepage';
+import activitiespage from './activitiespage';
 
 export const CloneDeep = (object) => lodash.CloneDeep(object);
 export const PlayVideo = (src) => homepage.playVideo(src);
@@ -7,3 +8,4 @@ export const FilterCarousel = () => homepage.filterCarousel();
 export const LogoBanerCarousel = () => homepage.logoBanerCarousel();
 export const GetUserLocation = () => homepage.getUserLocation();
 export const ShowResult = () => homepage.showResult();
+export const OtherAct = () => activitiespage.otherAct();

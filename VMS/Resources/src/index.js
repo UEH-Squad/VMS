4ad8067c96a4290610ec043558cf2f95855aaa00
Long Duong <longduong@kms-technology.com>
2021-08-20 21:10:25 +0700

    Remove redundant code

--- VMS/Resources/src/index.js
+++ VMS/Resources/src/index.js
@@ -1,6 +1,5 @@
 ﻿import lodash from './common/lodash';
 import homepage from './homepage';
-import activitySearch from './activitySearch';
 
 export const CloneDeep = (object) => lodash.CloneDeep(object);
 export const PlayVideo = (src) => homepage.playVideo(src);

5ac76996cf0baff15dfe64a60bc6d6b932663e44
Truckhang181001 <Truckhang181001@gmail.com>
2021-08-23 01:35:20 +0700

    Merge new code

--- VMS/Resources/src/index.js
+++ VMS/Resources/src/index.js
@@ -1,11 +1,11 @@
-﻿import lodash from './common/lodash';
-import homepage from './homepage';
+﻿import homepage from './homepage';
 import activitiespage from './activitiespage';
 
-export const CloneDeep = (object) => lodash.CloneDeep(object);
 export const PlayVideo = (src) => homepage.playVideo(src);
 export const FilterCarousel = () => homepage.filterCarousel();
 export const LogoBanerCarousel = () => homepage.logoBanerCarousel();
+export const SetUserLocation = () => homepage.setUserLocation();
 export const GetUserLocation = () => homepage.getUserLocation();
 export const ShowResult = () => homepage.showResult();
+export const IncreaseNumber1 = () => homepage.increaseNumber1();
 export const OtherAct = () => activitiespage.otherAct();

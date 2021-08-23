772956189ab5a33b68409d54eb381bf93f2cde93
truckhang181001 <85405909+truckhang181001@users.noreply.github.com>
2021-08-23 20:24:24 +0700

    Finish frontend of other-activities. I cant handle favorite icon clic… (#72)
    * Design navigation bar
    
    * Fix navigation not push content down
    
    * Draft design homepage
    
    * Use User as IdentityService
    
    * navigation bar (#44)
    
    * Lam-navbar-commit1
    
    * navbar second commit
    
    * fix coding conventions
    
    * navigation bar for login/register page
    
    * fix logout button when user signed in
    
    * fix link, add Routes, delete unnecessary comments
    
    * Completed Filter and logoBaner (#47)
    
    * completed story Filter and LogoBaner
    
    * fix story LogoBaner
    
    * fix story logoBaner part 2
    
    * fix err show correctly
    
    * Add Related Activities Section (#45)
    
    * Add related activities section
    
    * add get user location function
    
    * delete property
    
    * adjusted as Mr.Long's requirements
    
    * fix responsive
    
    * fix the position of class act-more-2
    
    * fix responsive
    
    * add overflow-hidden
    
    * add width
    
    * fix responsive
    
    * add width 100%
    
    * change the position of 'nhieu hon'
    
    * just save it the last time, nothing was changed
    
    * save code the last time
    
    * move related activities under filter
    
    * Add limited-text, change the position of "Nhieu hon" (#54)
    
    * add limited-text class
    
    * change position of 'nhieu hon'
    
    * Footer for mobile/tablet/pc (#49)
    
    * test
    
    * Footer for mobile/tablet/pc
    
    * create footer
    
    * footer for mobile/tablet/pc
    
    * get latest code
    
    * Homepage video (#50)
    
    * update design video header
    
    * final video-header
    
    * Final Homepage-video, revert file package-lock.json
    
    * Fix margin-top in breakpoint sm, removed title
    
    * fix js
    
    * Fix timer
    
    * Removed unnecessary file (_animation.scss), comments
    
    * Quote banner and introduce paragraph (#51)
    
    * QuoteBanner ver1
    
    * enhance showResult
    
    * refacter showResult
    
    * Add Related Activities Section
    
    * set margin top, margin bottom
    
    * modify position of quote line
    
    * Revert deleted code
    
    * Remove irrelevant border
    
    * Remove console.log
    
    Co-authored-by: Long Duong <longduong@kms-technology.com>
    
    * Fix Routes and margin-top
    
    * Design common layout
    
    * Design common layout
    
    * Unfixed-top when user in activity search page
    
    * Fix fixed-top navbar
    
    * Finish frontend of other-activities. I cant handle favorite icon click issue although i tried
    
    * Update local variables
    
    * update code css
    
    * Merge new code
    
    * Handle click on heart icon for other activities
    
    * Rename component
    
    Co-authored-by: Long Duong <huulongduong@gmail.com>
    Co-authored-by: Long Duong <longduong@kms-technology.com>
    Co-authored-by: ThanhLamUit <87577313+ThanhLamUit@users.noreply.github.com>
    Co-authored-by: ThaiNgocHuuBang <86879477+ThaiNgocHuuBang@users.noreply.github.com>
    Co-authored-by: lananhkn <87310006+lananhkn@users.noreply.github.com>
    Co-authored-by: Bùi Minh Kha <87795372+KhaB-BIT@users.noreply.github.com>
    Co-authored-by: hoanguyennph <87793229+hoanguyennph@users.noreply.github.com>

--- VMS/Resources/src/index.js
+++ VMS/Resources/src/index.js
@@ -1,9 +1,10 @@
 ﻿import homepage from './homepage';
+import activitiespage from './activityInfo';
 
 export const PlayVideo = (src) => homepage.playVideo(src);
 export const FilterCarousel = () => homepage.filterCarousel();
 export const LogoBanerCarousel = () => homepage.logoBanerCarousel();
 export const SetUserLocation = () => homepage.setUserLocation();
 export const GetUserLocation = () => homepage.getUserLocation();
-export const ShowResult = () => homepage.showResult();
 export const IncreaseNumber = () => homepage.increaseNumber();
+export const OtherAct = () => activitiespage.otherAct();

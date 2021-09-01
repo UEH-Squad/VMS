9c2e5d1381d794accbfbfa28b1281a8cd6b6540c
Bằng Thái Ngọc Hữu <thaingochuubang10@gmail.com>
2021-09-01 11:11:50 +0700

    get laster code

--- VMS/Resources/src/organizationProfile/index.js
+++ VMS/Resources/src/organizationProfile/index.js
@@ -13,4 +13,18 @@ const informationCarousel = () => {
     })
 }
 
-export default { informationCarousel };
+const organizeCarousel = () => {
+    $('.organize__carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        responsive: {
+            0: {
+                items: 4,
+                slideBy: 4,
+            }
+        }
+    })
+}
+
+export default { informationCarousel, organizeCarousel };

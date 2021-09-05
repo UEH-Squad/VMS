0bc8ebef8dd720788c35fa77f8ba6ca42e9b8a00
Long Duong <longduong@kms-technology.com>
2021-09-05 20:39:21 +0700

    Update styles

---
+++ VMS/Pages/Organization/Activities/CascadingAddressPicker.razor.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VMS.Application.Interfaces;
+using VMS.Domain.Models;
+
+namespace VMS.Pages.Organization.Activities
+{
+    public partial class CascadingAddressPicker : ComponentBase
+    {
+        [Inject]
+        private IAddressService AddressService { get; set; }
+
+        [Parameter]
+        public int ProvinceId { get; set; }
+
+        [Parameter]
+        public int DistrictId { get; set; }
+
+        [Parameter]
+        public int WardId { get; set; }
+
+        [Parameter]
+        public Action<int, int, int> OnAddressChanged { get; set; }
+
+        private string[] defaultValues;
+        private bool isCityShow;
+        private bool isDistrictShow;
+        private bool isWardShow;
+        private string cityChoosenValue;
+        private string districtChoosenValue;
+        private string wardChoosenValue;
+        private List<AddressPath> provinces = new();
+        private List<AddressPath> districts = new();
+        private List<AddressPath> wards = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            provinces = await AddressService.GetAllProvincesAsync();
+            defaultValues = new[]
+            {
+                "Tỉnh/Thành phố",
+                "Quận/Huyện",
+                "Phường/Xã"
+            };
+            cityChoosenValue = defaultValues[0];
+            districtChoosenValue = defaultValues[1];
+            wardChoosenValue = defaultValues[2];
+        }
+
+        private async Task ChooseCityValue(AddressPath addressPath)
+        {
+            ProvinceId = addressPath.Id;
+            cityChoosenValue = addressPath.Name;
+            ToggleCityDropdown();
+
+            districts = await AddressService.GetAllAddressPathsByParentIdAsync(addressPath.Id);
+            DistrictId = 0;
+            WardId = 0;
+            districtChoosenValue = defaultValues[1];
+            wardChoosenValue = defaultValues[2];
+
+            OnAddressChanged?.Invoke(ProvinceId, DistrictId, WardId);
+        }
+
+        private async Task ChooseDistrictValue(AddressPath addressPath)
+        {
+            DistrictId = addressPath.Id;
+            districtChoosenValue = addressPath.Name;
+            ToggleDistrictDropdown();
+
+            wards = await AddressService.GetAllAddressPathsByParentIdAsync(addressPath.Id);
+            WardId = 0;
+            wardChoosenValue = defaultValues[2];
+
+            OnAddressChanged?.Invoke(ProvinceId, DistrictId, WardId);
+        }
+
+        private void ChooseWardValue(AddressPath addressPath)
+        {
+            WardId = addressPath.Id;
+            wardChoosenValue = addressPath.Name;
+            ToggleWardDropdown();
+
+            OnAddressChanged?.Invoke(ProvinceId, DistrictId, WardId);
+        }
+
+        private void ToggleCityDropdown(bool isClose = false) => isCityShow = !isClose && !isCityShow;
+
+        private void ToggleDistrictDropdown(bool isClose = false) => isDistrictShow = !isClose && !isDistrictShow;
+
+        private void ToggleWardDropdown(bool isClose = false) => isWardShow = !isClose && !isWardShow;
+    }
+}
--- VMS/Pages/Organization/Activities/CreateEditActivity.razor.cs
+++ VMS/Pages/Organization/Activities/CreateEditActivity.razor.cs
@@ -17,9 +17,6 @@ namespace VMS.Pages.Organization.Activities
         [CascadingParameter] public IModalService Modal { get; set; }
 
         [Inject]
-        private IAddressService AddressService { get; set; }
-
-        [Inject]
         private IJSRuntime JSRuntime { get; set; }
 
         [Inject]
@@ -31,88 +28,19 @@ namespace VMS.Pages.Organization.Activities
             EndDate = DateTime.Now.AddDays(7)
         };
 
-        private bool isCityShow;
-        private bool isDistrictShow;
-        private bool isWardShow;
-        private string cityChoosenValue = "Tỉnh/Thành phố";
-        private string districtChoosenValue = "Quận/Huyện";
-        private string wardChoosenValue = "Phường/Xã";
-        private List<AddressPath> provinces;
-        private List<AddressPath> districts;
-        private List<AddressPath> wards;
-
         private readonly List<string> targets = new()
         {
             "Sinh viên năm nhất",
             "Sinh viên năm hai",
             "Sinh viên năm ba",
-            "Sinh viên năm tư"
+            "Tất cả mọi đối tượng"
         };
 
-        protected override async Task OnInitializedAsync()
+        private void OnAddressChanged(int provinceId, int districtId, int wardId)
         {
-            provinces = await AddressService.GetAllProvincesAsync();
-        }
-
-        private async Task ChooseCityValue(AddressPath addressPath)
-        {
-            activity.ProvinceId = addressPath.Id;
-            cityChoosenValue = addressPath.Name;
-            ToggleCityDropdown();
-
-            districts = await AddressService.GetAllAddressPathsByParentIdAsync(addressPath.Id);
-            activity.DistrictId = 0;
-            activity.WardId = 0;
-            districtChoosenValue = "Quận/Huyện";
-            wardChoosenValue = "Phường/Xã";
-        }
-
-        private async Task ChooseDistrictValue(AddressPath addressPath)
-        {
-            activity.DistrictId = addressPath.Id;
-            districtChoosenValue = addressPath.Name;
-            ToggleDistrictDropdown();
-
-            wards = await AddressService.GetAllAddressPathsByParentIdAsync(addressPath.Id);
-            activity.WardId = 0;
-            wardChoosenValue = "Phường/Xã";
-        }
-
-        private void ChooseWardValue(AddressPath addressPath)
-        {
-            activity.WardId = addressPath.Id;
-            wardChoosenValue = addressPath.Name;
-            ToggleWardDropdown();
-        }
-
-        private void ToggleCityDropdown()
-        {
-            isCityShow = !isCityShow;
-        }
-
-        private void ToggleDistrictDropdown()
-        {
-            isDistrictShow = !isDistrictShow;
-        }
-
-        private void ToggleWardDropdown()
-        {
-            isWardShow = !isWardShow;
-        }
-
-        private void CloseCityDropdown()
-        {
-            isCityShow = false;
-        }
-
-        private void CloseDistrictDropdown()
-        {
-            isDistrictShow = false;
-        }
-
-        private void CloseWardDropdown()
-        {
-            isWardShow = false;
+            activity.ProvinceId = provinceId;
+            activity.DistrictId = districtId;
+            activity.WardId = wardId;
         }
 
         private async Task OnStartDateChanged(ChangeEventArgs args)
@@ -126,6 +54,7 @@ namespace VMS.Pages.Organization.Activities
 
             activity.StartDate = selectedDate;
         }
+
         private async Task OnEndDateChanged(ChangeEventArgs args)
         {
             DateTime selectedDate = DateTime.Parse(args.Value.ToString());
@@ -140,32 +69,30 @@ namespace VMS.Pages.Organization.Activities
 
         private async Task ShowAreasPopupAsync()
         {
-            var parameters = new ModalParameters();
+            ModalParameters parameters = new();
             parameters.Add("ChoosenAreasList", activity.Areas);
 
-            var options = new ModalOptions()
-            {
-                HideCloseButton = true,
-                DisableBackgroundCancel = true,
-                UseCustomLayout = true
-            };
-
-            await Modal.Show<ActivitySearchPage.AreasPopup>("", parameters, options).Result;
+            await ShowModalAsync(typeof(ActivitySearchPage.AreasPopup), parameters);
         }
 
         private async Task ShowSkillsPopup()
         {
-            var skillsParameter = new ModalParameters();
-            skillsParameter.Add("ChoosenSkillsList", activity.Skills);
+            var parameters = new ModalParameters();
+            parameters.Add("ChoosenSkillsList", activity.Skills);
 
-            var options = new ModalOptions()
+            await ShowModalAsync(typeof(ActivitySearchPage.SkillsPopup), parameters);
+        }
+
+        private async Task ShowModalAsync(Type type, ModalParameters parameters)
+        {
+            ModalOptions options = new()
             {
                 HideCloseButton = true,
                 DisableBackgroundCancel = true,
                 UseCustomLayout = true
             };
 
-            await Modal.Show<ActivitySearchPage.SkillsPopup>("", skillsParameter, options).Result;
+            await Modal.Show(type, "", parameters, options).Result;
         }
 
         private async Task<IEnumerable<string>> SearchTargets(string searchText)
@@ -177,15 +104,15 @@ namespace VMS.Pages.Organization.Activities
         {
             return await SkillService.GetAllSkillsByNameAsync(searchText);
         }
+    }
 
-        private class Fake : Activity
-        {
-            public int ProvinceId { get; set; }
-            public int DistrictId { get; set; }
-            public int WardId { get; set; }
-            public IList<SkillViewModel> Skills { get; set; } = new List<SkillViewModel>();
-            public IList<AreaViewModel> Areas { get; set; } = new List<AreaViewModel>();
-            public IList<string> Targets { get; set; }
-        }
+    public class Fake : Activity
+    {
+        public int ProvinceId { get; set; }
+        public int DistrictId { get; set; }
+        public int WardId { get; set; }
+        public IList<SkillViewModel> Skills { get; set; } = new List<SkillViewModel>();
+        public IList<AreaViewModel> Areas { get; set; } = new List<AreaViewModel>();
+        public IList<string> Targets { get; set; }
     }
 }

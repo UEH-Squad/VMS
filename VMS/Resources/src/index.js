0079157312ae0d4275fde1f2caacc3153aa7828d
Dương Hữu Long <huulongduong@gmail.com>
2021-09-11 12:11:45 +0700

    Create/edit activity pages (#112)
    * handle logic for filter bar
    
    * fix sub skill popup
    
    * add new table, column and handle logic
    
    * handle logic for featured activities
    
    * handle logic for order activities
    
    * Fix Pager, Scroll to top and memory leak
    
    * fix indentation
    
    * fix and handle fav activity
    
    * revert images committed
    
    * Revert "revert images committed"
    
    This reverts commit a2009b227dd05c34446d80c7a5b2dd84931564b9.
    
    * Revert "fix and handle fav activity"
    
    This reverts commit 996dfebd496af38207120766b6a7ff346e89897e.
    
    * revert code
    
    * Revert "fix and handle fav activity"
    
    This reverts commit 996dfebd496af38207120766b6a7ff346e89897e.
    
    * revert committ, so chaotic
    
    * Revert "fix and handle fav activity"
    
    This reverts commit 996dfebd496af38207120766b6a7ff346e89897e.
    
    * revert again, hate git
    
    * delete images
    
    * handle signup activity logic
    
    * fix signup activity logic
    
    * rename method follow coding convention
    
    * fix url featured activity
    
    * fix activity organizer name
    
    * Code up the base component
    
    * fix logic code
    
    * change some code
    
    * fix onfocusout filterbar
    
    * fix showsubskill show up and hidden
    
    * fix skill popup style css
    
    * fix checkbox uncheck when clear filter
    
    * fix org name in drop-downlist filter bar
    
    * fix signup bug
    
    * fix area popup and skill popup
    
    * remove space redundancy
    
    * fix featured activity hover
    
    * fix feedback
    
    * connect homepage and activity search page
    
    * connect area in homepage and activity search page
    
    * remove unnecessary method
    
    * add NavigationManagerExtensions
    
    * handle query string
    
    * change orderList type from bool[] to Dictionary<string, bool>
    
    * fix feedback
    
    * Fix UI and split components
    
    * Restructure code
    
    * fix name convention
    
    * fix comment feedback
    
    * remove unnecessary method
    
    * Refactor UI and code
    
    * Update styles
    
    * Fix build error
    
    * Adjust Activities table
    
    * Handle submit and invalidsubmit
    
    * Fix upload image and styles
    
    * Enhance and implement preview image
    
    * Split spinner to separate component
    
    * Refactor code
    
    * Add comments
    
    * Move UploadImg to shared components folder
    
    * Refactor js code
    
    * delete redundant files
    
    * Return newly created activity id
    
    * Minor fixes
    
    * Minor fix styles
    
    * Check for MIME types fix styles
    
    * Refactor js
    
    * Redirect pages
    
    * Create activities folder to save images if not exists
    
    * Remove already-defined cols
    
    Co-authored-by: HoaiTran308 <tranvanhoai.tt4@gmail.com>
    Co-authored-by: Long Duong <longduong@kms-technology.com>
    Co-authored-by: KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>

--- VMS/Resources/src/index.js
+++ VMS/Resources/src/index.js
@@ -1,8 +1,9 @@
-﻿import { smoothScrollTo } from './common';
+﻿import { smoothScrollTo, hookFileUploadEvent } from './common';
 import homepage from './homepage';
 import activitiespage from './activityInfo';
 
 export const SmoothScrollTo = (element) => smoothScrollTo(element);
+export const HookFileUploadEvent = (previewImg, fileUploadRefId, discardBtn, imgContainerId, originalSrc) => hookFileUploadEvent(previewImg, fileUploadRefId, discardBtn, imgContainerId, originalSrc);
 export const PlayVideo = (src) => homepage.playVideo(src);
 export const FilterCarousel = () => homepage.filterCarousel();
 export const LogoBanerCarousel = () => homepage.logoBanerCarousel();
---
+++ VMS/Shared/Components/UploadImg.razor.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace VMS.Shared.Components
+{
+    /// <summary>
+    /// UploadImg component
+    /// </summary>
+    public partial class UploadImg : ComponentBase
+    {
+        [Inject] private IJSRuntime JS { get; set; }
+
+        #region Parameters
+
+        /// <summary>
+        /// Initial prompt text when the whole component shown up
+        /// </summary>
+        [Parameter]
+        public string DefaultPrompt { get; set; } = "Chọn hoặc kéo và thả hình ảnh liên quan tại đây!";
+
+        /// <summary>
+        /// Toggle the preview img panel
+        /// </summary>
+        [Parameter]
+        public bool ShowPreview { get; set; }
+
+        /// <summary>
+        /// The src attribute value of the &lt;img&gt; used in preview
+        /// </summary>
+        [Parameter]
+        public string PreviewImgSrc { get; set; }
+
+        /// <summary>
+        /// file filter when choosing file via open file dialog of operating system, default is "image/*"
+        /// </summary>
+        [Parameter]
+        public string AcceptPattern { get; set; } = "image/*";
+
+        /// <summary>
+        /// Upload File selection change event
+        /// </summary>
+        [Parameter]
+        public EventCallback<InputFileChangeEventArgs> InputFileChanged { get; set; }
+
+        [Parameter]
+        public Func<Task> ImageDiscarded { get; set; }
+
+        #endregion Parameters
+
+        private string prompt;
+        private string fileName;
+        private string previewImgAltText;
+
+        private readonly string fileUploadId = Guid.NewGuid().ToString();
+        private readonly string imgContainerId = Guid.NewGuid().ToString();
+        private ElementReference previewImg;
+        private ElementReference discardBtn;
+
+        protected override void OnInitialized()
+        {
+            if (string.IsNullOrEmpty(prompt) && !string.IsNullOrEmpty(DefaultPrompt))
+            {
+                prompt = DefaultPrompt;
+            }
+        }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (ShowPreview)
+            {
+                await JS.InvokeVoidAsync("vms.HookFileUploadEvent", previewImg, fileUploadId, discardBtn, imgContainerId, PreviewImgSrc);
+            }
+        }
+
+        private async Task OnInputFileChange(InputFileChangeEventArgs e)
+        {
+            previewImgAltText = e.File.Name;
+            if (ShowPreview)
+            {
+                fileName = previewImgAltText;
+            }
+            else
+            {
+                prompt = previewImgAltText;
+            }
+
+            await InputFileChanged.InvokeAsync(e);
+        }
+
+        private void OnDiscardImage()
+        {
+            previewImgAltText = fileName = null;
+            ImageDiscarded?.Invoke();
+        }
+    }
+}

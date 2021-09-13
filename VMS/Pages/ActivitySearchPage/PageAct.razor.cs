e74ed38741be2533c726451b5c32d589990a4223
Dương Hữu Long <huulongduong@gmail.com>
2021-09-13 21:30:32 +0700

    [Backend] Use spatial datatype for the activity location (#123)
    * Add Location spatial type
    
    * Fix logic to use spatial datatype
    
    * Add comments
    
    * Revert db name
    
    Co-authored-by: Long Duong <longduong@kms-technology.com>

--- VMS/Pages/ActivitySearchPage/PageAct.razor.cs
+++ VMS/Pages/ActivitySearchPage/PageAct.razor.cs
@@ -2,6 +2,7 @@
 using Blazored.Modal.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using NetTopologySuite.Geometries;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 using VMS.Application.Interfaces;
 using VMS.Application.ViewModels;
 using VMS.Common;
+using VMS.Common.Extensions;
 using VMS.Domain.Models;
 
 namespace VMS.Pages.ActivitySearchPage
@@ -17,7 +19,7 @@ namespace VMS.Pages.ActivitySearchPage
     {
         private User currentUser;
         private int page = 1;
-        private Coordinate location;
+        private Coordinate userCoordinate;
         private IEnumerable<ActivityViewModel> featuredActivities;
         private PagedResult<ActivityViewModel> pagedResult = new() { Results = new() };
 
@@ -51,11 +53,12 @@ namespace VMS.Pages.ActivitySearchPage
         {
             featuredActivities = await ActivityService.GetFeaturedActivitiesAsync();
 
-            location = await JsRuntime.InvokeAsync<Coordinate>("vms.GetUserLocation");
-            if (location is null)
+            var userLocation = await JsRuntime.InvokeAsync<CoordinateJs>("vms.GetUserLocation");
+            userCoordinate = userLocation.ToCoordinate();
+            if (userCoordinate is null)
             {
                 string userAddresses = IdentityService.GetCurrentUserAddress();
-                location = await AddressLocationService.GetCoordinateAsync(userAddresses);
+                userCoordinate = await AddressLocationService.GetCoordinateAsync(userAddresses);
             }
 
             currentUser = IdentityService.GetCurrentUserWithFavoritesAndRecruitments();
@@ -63,12 +66,12 @@ namespace VMS.Pages.ActivitySearchPage
 
         protected override async Task OnParametersSetAsync()
         {
-            pagedResult = await ActivityService.GetAllActivitiesAsync(IsSearch, SearchValue, Filter, OrderList, location, page);
+            pagedResult = await ActivityService.GetAllActivitiesAsync(IsSearch, SearchValue, Filter, OrderList, userCoordinate, page);
         }
 
         private async Task HandlePageChanged()
         {
-            pagedResult = await ActivityService.GetAllActivitiesAsync(IsSearch, SearchValue, Filter, OrderList, location, page);
+            pagedResult = await ActivityService.GetAllActivitiesAsync(IsSearch, SearchValue, Filter, OrderList, userCoordinate, page);
             StateHasChanged();
             await Interop.ScrollToTop(JsRuntime);
         }

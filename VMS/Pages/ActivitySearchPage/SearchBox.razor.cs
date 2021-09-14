6577719008590dcbd6a0ef58362eae8704183f23
ThanhLam <thanhlam.6178@gmail.com>
2021-09-14 20:12:31 +0700

    create SearchBox component (css only)

---
+++ VMS/Pages/ActivitySearchPage/SearchBox.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VMS.Application.Interfaces;
+using VMS.Application.ViewModels;
+using VMS.Domain.Models;
+
+namespace VMS.Pages.ActivitySearchPage
+{
+    public partial class SearchBox
+    {
+        [Parameter]
+        public string SearchValue { get; set; }
+
+        [Parameter]
+        public EventCallback<string> SearchValueChanged { get; set; }
+
+        private void UpdateSearchValueAsync(ChangeEventArgs e)
+        {
+            SearchValue = (string)e.Value;
+        }
+
+        private async Task SearchAsync()
+        {
+            await SearchValueChanged.InvokeAsync(SearchValue);
+        }
+
+        private void ClearSearchBox()
+        {
+            SearchValue = string.Empty;
+        }
+    }
+}

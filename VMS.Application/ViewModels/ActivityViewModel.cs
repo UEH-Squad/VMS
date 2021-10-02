4d59239cd821fac358da5ceb6a4c763ea30a4e12
AnhTuanLam <tuanlam.31191026900@st.ueh.edu.vn>
2021-10-02 22:54:46 +0700

    link org page to place related

--- VMS.Application/ViewModels/ActivityViewModel.cs
+++ VMS.Application/ViewModels/ActivityViewModel.cs
@@ -7,6 +7,7 @@ namespace VMS.Application.ViewModels
     public class ActivityViewModel
     {
         public int Id { get; set; }
+        public string OrgId { get; set; }
         public int AreaId { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }

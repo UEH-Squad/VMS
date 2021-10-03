4f36c8b893f785d94499de3774488cfa42e1eb8a
Long Duong <longduong@kms-technology.com>
2021-10-03 10:57:01 +0700

    Re-structure folders

---
+++ VMS/Pages/Volunteer/EditUserProfile/Index.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VMS.Application.Interfaces;
+using VMS.Application.ViewModels;
+
+namespace VMS.Pages.Volunteer.EditUserProfile
+{
+    public partial class Index
+    {
+        [Inject]
+        private ISkillService SkillService { get; set; }
+
+        private async Task<IEnumerable<SkillViewModel>> SearchSkills(string searchText)
+        {
+            return await SkillService.GetAllSkillsByNameAsync(searchText);
+        }
+    }
+}

f94121f103acd310c946a824add1e9e5367dcef2
HIEU <hhhhhieu102@gmail.com>
2021-10-16 21:31:11 +0700

    handle filter (semester & faculty)

---
+++ VMS/Pages/ActivityLogPage/Filter.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VMS.Application.Interfaces;
+using VMS.Application.ViewModels;
+
+namespace VMS.Pages.ActivityLogPage
+{
+    public partial class Filter
+    {
+        [Inject]
+        private IFacultyService FacultyService { get; set; }
+
+        private string facultyDefault = "Đơn vị";
+        private string semesterDefault = "Học kỳ";
+        private List<FacultyViewModel> faculties = new();
+
+        private readonly List<string> semesters = new()
+        {
+            "Học kỳ đầu",
+            "Học kỳ giữa",
+            "Học kỳ cuối"
+        };
+
+        protected override async Task OnInitializedAsync()
+        {
+            faculties = await FacultyService.GetAllFacultiesAsync();
+        }
+
+        private void ChooseSemester(string semester)
+        {
+            semesterDefault = semester;
+            Display1 = "d-none";
+        }
+
+        private void ChooseFaculty(FacultyViewModel faculty)
+        {
+            facultyDefault = faculty.Name;
+            Display2 = "d-none";
+        }
+
+    }
+}

c6df49ef76320fbadd79b50f3121613416890c96
Trần Văn Hoài <76389706+hoaitran308@users.noreply.github.com>
2021-11-01 12:00:18 +0700

    [Backend] Handle logic for rating page (#145)
    * Done Rating Page
    
    * fix UI and radio button
    
    * implement 2-way binding to get rating star
    
    * get value from FilterRatingAll and pass in Stars
    
    * fix star rating
    
    * replace searchbox, fix scss for filter
    
    * fix UI and remove redundant code
    
    * modify popup report sucess
    
    * change position of SearchResult.razor
    
    * handle check role and org id
    
    * handle logic for rating user
    
    * handle multiple rating
    
    * handle logic for rate all
    
    * handle logic for search box and filter bar
    
    * update GetAllRecruitmentsAsync method
    
    * handle comment popup
    
    * handle logic for comment
    
    * fix logic to handle comment
    
    * Remove right-rail of register page
    
    * fix button state
    
    * fix update recruitments
    
    * fix size of avatar of member
    
    * fix border radius for avatar
    
    * fix border radius again
    
    * fix fit of avatar
    
    * change <p >tag to <a>  tag for user name and css for <a> tag
    
    * fix logic for get user by include, not use identity anymore
    
    * hot fix paging
    
    * fix and remove unnecessary things.
    
    Co-authored-by: HoaNguyenPhu <hoanguyen2002.nph@gmail.com>
    Co-authored-by: Long Duong <longduong@kms-technology.com>
    Co-authored-by: Dương Hữu Long <huulongduong@gmail.com>

---
+++ VMS.Application/ViewModels/RecruitmentRatingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace VMS.Application.ViewModels
+{
+    public class RecruitmentRatingViewModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public double Rank { get; set; }
+        public string Comment {  get; set; }
+        public bool IsOrgRating { get; set; }
+    }
+}
---
+++ VMS/Pages/Organization/RatingPage/Index.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using VMS.Application.Interfaces;
+using VMS.Application.ViewModels;
+using VMS.Common;
+using VMS.Domain.Models;
+
+namespace VMS.Pages.Organization.RatingPage
+{
+    public partial class Index : ComponentBase
+    {
+        private bool? isRated;
+        private string searchValue;
+        private int starRating;
+        private User currentUser;
+        private ViewActivityViewModel activity;
+
+        [Parameter] public int ActivityId { get; set; }
+
+        [Inject]
+        private IIdentityService IdentityService { get; set; }
+        [Inject]
+        private IActivityService ActivityService { get; set; }
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            currentUser = IdentityService.GetCurrentUser();
+
+            activity = await ActivityService.GetViewActivityViewModelAsync(ActivityId);
+
+            if (activity.OrgId != currentUser.Id)
+            {
+                NavigationManager.NavigateTo(Routes.HomePage, true);
+            }
+        }
+
+        private void IsRatedChanged(bool? value)
+        {
+            isRated = value;
+            searchValue = string.Empty;
+        }
+
+        private void SearchValueChanged(string value)
+        {
+            searchValue = value;
+            isRated = null;
+        }
+    }
+}

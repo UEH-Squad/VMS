d89bdfc9cb6814b8abde85a2312f86c9eaad1c0b
HIEU <hhhhhieu102@gmail.com>
2021-11-07 23:47:11 +0700

    add column NotifiedEmail and handle notification email

--- VMS.Application/ViewModels/CreateUserProfileViewModel.cs
+++ VMS.Application/ViewModels/CreateUserProfileViewModel.cs
@@ -13,14 +13,14 @@ namespace VMS.Application.ViewModels
         public string Id { get; set; }
         [Required] public string FullName { get; set; }
         [Required] public string UserName { get; set; }
-        [Required] public string Email { get; set; }
+        [Required] public string NotifiedEmail { get; set; }
         [Required] public string PhoneNumber { get; set; }
 
         public string Banner { get; set; }
         public string Avatar { get; set; }
 
         [RequiredHasItems]
-        public IList<AreaViewModel> Areas { get; set; } = new List<AreaViewModel>();      
+        public IList<AreaViewModel> Areas { get; set; } = new List<AreaViewModel>();
     }
 
     public class CreateOrgProfileViewModel : UserProfileViewModel
---
+++ VMS.Infrastructure.Data/Migrations/20211107163352__Add-Column-NotifiedEmail.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VMS.Infrastructure.Data.Migrations
+{
+    public partial class _AddColumnNotifiedEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NotifiedEmail",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e570",
+                column: "ConcurrencyStamp",
+                value: "a3c22b46-7eb7-45e1-9516-89e04d7a4398");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e571",
+                column: "ConcurrencyStamp",
+                value: "494c3f5d-473d-4842-aaa0-5617fc28c807");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e572",
+                column: "ConcurrencyStamp",
+                value: "90ad50b2-41aa-4671-9b05-ae59a3113510");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotifiedEmail",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e570",
+                column: "ConcurrencyStamp",
+                value: "445034e8-bea2-4693-803e-811277f3a2de");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e571",
+                column: "ConcurrencyStamp",
+                value: "d2ac63c5-f33e-4602-8a3b-b050359cbb08");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e572",
+                column: "ConcurrencyStamp",
+                value: "5b73e236-a1fa-484e-95fc-0311f1c3a683");
+        }
+    }
+}

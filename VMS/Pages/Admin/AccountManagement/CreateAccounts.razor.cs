f70ca65346ee41901fdfb683c7772800f5080025
HoaiTran308 <tranvanhoai.tt4@gmail.com>
2021-11-13 11:17:33 +0700

    handle download demo import file

--- VMS/Pages/Admin/AccountManagement/CreateAccounts.razor.cs
+++ VMS/Pages/Admin/AccountManagement/CreateAccounts.razor.cs
@@ -1,9 +1,12 @@
-﻿using VMS.Common;
+﻿using System.IO;
+using VMS.Common;
 using Blazored.Modal;
 using VMS.Common.Enums;
+using Microsoft.JSInterop;
 using System.Threading.Tasks;
 using Blazored.Modal.Services;
 using VMS.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
@@ -20,6 +23,8 @@ namespace VMS.Pages.Admin.AccountManagement
         [CascadingParameter] public IModalService ResultModal { get; set; }
 
         [Inject] private IExcelService ExcelService { get; set; }
+        [Inject] private IWebHostEnvironment WebHostEnvironment { get; set; }
+        [Inject] private IJSRuntime JSRuntime { get; set; }
 
         private async Task CloseModalAsync()
         {
@@ -47,6 +52,17 @@ namespace VMS.Pages.Admin.AccountManagement
             ModalParameters parameters = new();
             parameters.Add("IsSuccess", isSuccess);
             ResultModal.Show<CreateFailed>("", parameters, BlazoredModalOptions.GetModalOptions());
+        }
+
+        private async Task OnClickDownloadDemo()
+        {
+            string fileName = Role == Role.User ? "IMPORT_USER_DEMO.xlsx" : "IMPORT_ORG_DEMO.xlsx";
+
+            string filePath = Path.Combine(WebHostEnvironment.WebRootPath, "files", "admin", fileName);
+
+            byte[] fileBytes = File.ReadAllBytes(filePath);
+
+            await JSRuntime.InvokeVoidAsync("vms.SaveAsFile", fileName, fileBytes);
         }
     }
 }
